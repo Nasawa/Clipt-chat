8be9604a8730fd34bbb6ffd1cedafc8c202bd1ab
cjohnsonal <christopher.johnson@medicaid.alabama.gov>
2015-07-09 14:35:49 -0500

    init

---
+++ Clipt Chat/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace Clipt_Chat
+{
+    partial class ChatWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textServer = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textRoom = new System.Windows.Forms.TextBox();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.chatOutput = new System.Windows.Forms.RichTextBox();
+            this.chatInput = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textUser = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textServer
+            // 
+            this.textServer.Location = new System.Drawing.Point(12, 25);
+            this.textServer.Name = "textServer";
+            this.textServer.Size = new System.Drawing.Size(203, 20);
+            this.textServer.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Server";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(221, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Room";
+            // 
+            // textRoom
+            // 
+            this.textRoom.Location = new System.Drawing.Point(221, 25);
+            this.textRoom.Name = "textRoom";
+            this.textRoom.Size = new System.Drawing.Size(90, 20);
+            this.textRoom.TabIndex = 1;
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnConnect.Location = new System.Drawing.Point(408, 25);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(64, 20);
+            this.btnConnect.TabIndex = 3;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // chatOutput
+            // 
+            this.chatOutput.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.chatOutput.Location = new System.Drawing.Point(12, 52);
+            this.chatOutput.Name = "chatOutput";
+            this.chatOutput.Size = new System.Drawing.Size(460, 197);
+            this.chatOutput.TabIndex = 5;
+            this.chatOutput.Text = "";
+            // 
+            // chatInput
+            // 
+            this.chatInput.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.chatInput.Location = new System.Drawing.Point(12, 255);
+            this.chatInput.Name = "chatInput";
+            this.chatInput.Size = new System.Drawing.Size(387, 20);
+            this.chatInput.TabIndex = 4;
+            // 
+            // btnSend
+            // 
+            this.btnSend.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSend.Location = new System.Drawing.Point(408, 255);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(64, 20);
+            this.btnSend.TabIndex = 5;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(315, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Username";
+            // 
+            // textUser
+            // 
+            this.textUser.Location = new System.Drawing.Point(315, 25);
+            this.textUser.Name = "textUser";
+            this.textUser.Size = new System.Drawing.Size(90, 20);
+            this.textUser.TabIndex = 2;
+            // 
+            // ChatWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 285);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textUser);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.chatInput);
+            this.Controls.Add(this.chatOutput);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textRoom);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textServer);
+            this.Name = "ChatWindow";
+            this.Text = "Chat";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textServer;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textRoom;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.RichTextBox chatOutput;
+        private System.Windows.Forms.TextBox chatInput;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textUser;
+    }
+}
+
---
+++ Clipt Chat/Form1.cs
@@ -0,0 +1,124 @@
+﻿using Quobject.SocketIoClientDotNet.Client;
+using System;
+using System.Media;
+using System.Windows.Forms;
+
+namespace Clipt_Chat
+{
+    public partial class ChatWindow : Form
+    {
+        private string[] arr = { "Connect", "Disconnect", "http://", "Chat", "GLOBAL", "has joined the chat on", "id", "message", "Disconnected from server" };
+        private string id = null;
+
+        private Socket socket = null;
+
+        private string user = null;
+        public ChatWindow()
+        {
+            InitializeComponent();
+            FillData();
+        }
+
+        private delegate void WriteLogCallback(string log);
+
+        private enum strings : int
+        {
+            Connect, Disconnect, http, chat, global, joined, id, msg, left
+        };
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            if (socket != null)
+            {
+                socket.Disconnect();
+                socket = null;
+                btnConnect.Text = E(strings.Connect);
+                WriteLog(E(strings.left));
+            }
+            else
+            {
+                if (btnConnect.Text == E(strings.Disconnect))
+                    return;
+                btnConnect.Text = E(strings.Disconnect);
+                chatOutput.Clear();
+                id = textRoom.Text;
+                user = textUser.Text;
+                if (textServer.Text.Substring(0, 7) != E(strings.http))
+                    textServer.Text = String.Format("{0}{1}", E(strings.http), textServer.Text);
+                SaveData();
+                this.Text = String.Format("{0} - #{1}", E(strings.chat), id);
+                socket = IO.Socket(textServer.Text);
+                socket.Once(Socket.EVENT_CONNECT, () =>
+                {
+                    socket.Emit(E(strings.msg), id, string.Format("{0}: {1} {2} #{3}", E(strings.global), user, E(strings.joined), id));
+                });
+                socket.On(E(strings.id), (sid) =>
+                {
+                    socket.On(id + "", (msg) =>
+                    {
+                        WriteLog(msg.ToString());
+                    });
+
+                    chatInput.Enter += chatInput_Enter;
+                    chatInput.Leave += chatInput_Leave;
+                });
+            }
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            if (socket != null && chatInput.TextLength > 0)
+            {
+                socket.Emit(E(strings.msg), id, string.Format("{0}: {1}", user, chatInput.Text));
+                chatInput.ResetText();
+            }
+        }
+
+        private void chatInput_Enter(object sender, EventArgs e)
+        {
+            ActiveForm.AcceptButton = btnSend;
+        }
+
+        private void chatInput_Leave(object sender, EventArgs e)
+        {
+            ActiveForm.AcceptButton = null;
+        }
+
+        private string E(strings s)
+        {
+            return arr[(int)s];
+        }
+        private void FillData()
+        {
+            if (string.IsNullOrEmpty(textServer.Text))
+                textServer.Text = Properties.Settings.Default.Server;
+            if (string.IsNullOrEmpty(textRoom.Text))
+                textRoom.Text = Properties.Settings.Default.Room;
+            if (string.IsNullOrEmpty(textUser.Text))
+                textUser.Text = Properties.Settings.Default.User;
+        }
+
+        private void SaveData()
+        {
+            Properties.Settings.Default.Server = textServer.Text;
+            Properties.Settings.Default.Room = textRoom.Text;
+            Properties.Settings.Default.User = textUser.Text;
+            Properties.Settings.Default.Save();
+        }
+
+        private void WriteLog(string log)
+        {
+            if (log.Split(':')[0] != user)
+                SystemSounds.Beep.Play();
+            if (this.chatOutput.InvokeRequired)
+            {
+                WriteLogCallback d = new WriteLogCallback(WriteLog);
+                this.Invoke(d, new object[] { log });
+            }
+            else
+            {
+                this.chatOutput.AppendText(log + "\n");
+                this.chatOutput.ScrollToCaret();
+            }
+        }
+    }
+}
